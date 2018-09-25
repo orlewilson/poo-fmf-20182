1d7ac5b053b6966067f0555b68b08acfbb96e3f7
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2018-09-25 13:57:53 -0400

    aplicando o conceito de encapsulamento na class

--- Exemplo Classe/src/Personagem.java
+++ Exemplo Classe/src/Personagem.java
@@ -3,6 +3,7 @@
  * 	Autor (a): 	Orlewilson Bentes Maia
  *	Data:		31/08/2018 
  *	Descrição:	Exemplo de Classe abstraindo de um personagem de videogame
+ *				Encasulamento (private nos atributos e métodos get/set)
  * 
  */
 
@@ -11,8 +12,113 @@ public class Personagem {
 
 	// atributos (características)
 	// Atributos do tipo String recebem valores entre " "
-	String 	nome, cor, cabelo, roupa, olhos, dataNascimento, email, telefone;
-	
+	private String nome, cor, cabelo, roupa, olhos, dataNascimento, email, telefone;
+
 	// Atributos do tipo boolean recebem valores true ou false
-	boolean bigode, sapato, luvas, botoes, chapeu;
+	private boolean bigode, sapato, luvas, botoes, chapeu;
+
+	// métodos get (recuperar) e set (aterar)
+	public void setNome(String novoNome) {
+		nome = novoNome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setCor(String novaCor) {
+		cor = novaCor;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCabelo(String novoCabelo) {
+		cabelo = novoCabelo;
+	}
+
+	public String getCabelo() {
+		return cabelo;
+	}
+
+	public void setRoupa(String novaRoupa) {
+		roupa = novaRoupa;
+	}
+
+	public String getRoupa() {
+		return roupa;
+	}
+
+	public void setOlhos(String novosOlhos) {
+		olhos = novosOlhos;
+	}
+
+	public String getOlhos() {
+		return olhos;
+	}
+
+	public void setDataNascimento(String novaDataNascimento) {
+		dataNascimento = novaDataNascimento;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setEmail(String novoEmail) {
+		email = novoEmail;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setTelefone(String novoTelefone) {
+		email = novoTelefone;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setBigode(boolean novoBigode) {
+		bigode = novoBigode;
+	}
+
+	public boolean getBigode() {
+		return bigode;
+	}
+
+	public void setSapato(boolean novoSapato) {
+		bigode = novoSapato;
+	}
+
+	public boolean getSapato() {
+		return sapato;
+	}
+
+	public void setLuvas(boolean novasLuvas) {
+		luvas = novasLuvas;
+	}
+
+	public boolean getLuvas() {
+		return luvas;
+	}
+
+	public void setBotoes(boolean novosBotoes) {
+		luvas = novosBotoes;
+	}
+
+	public boolean getBotoes() {
+		return botoes;
+	}
+
+	public void setChapeu(boolean novoChapeu) {
+		chapeu = novoChapeu;
+	}
+
+	public boolean getChapeu() {
+		return chapeu;
+	}
 }
--- Exemplo Classe/src/Principal.java
+++ Exemplo Classe/src/Principal.java
@@ -13,69 +13,133 @@ public class Principal {
 
 		// criando objetos
 		Personagem mario = new Personagem();
-
 		Personagem megaman = new Personagem();
 
+		// antes do encapsulamento
+		// // atribuindo valores as características do objeto Mario
+		// mario.nome = "Mario Bros";
+		// mario.cor = "branco";
+		// mario.cabelo = "preto";
+		// mario.roupa = "vermelha";
+		// mario.olhos = "castanhos";
+		// mario.dataNascimento = "14/07/1983";
+		// mario.email = "mario@gmail.com";
+		// mario.telefone = "1234-1234";
+		// mario.bigode = true;
+		// mario.sapato = true;
+		// mario.luvas = true;
+		// mario.botoes = true;
+		// mario.chapeu = true;
+		//
+		// // mostrar valores dos atributos no console
+		// System.out.println("----- Personagem Mario -----");
+		// System.out.println("Nome: " + mario.nome);
+		// System.out.println("Cor: " + mario.cor);
+		// System.out.println("Cabelo: " + mario.cabelo);
+		// System.out.println("Roupa: " + mario.roupa);
+		// System.out.println("Olhos: " + mario.olhos);
+		// System.out.println("Data Nascimento: " + mario.dataNascimento);
+		// System.out.println("E-mail: " + mario.email);
+		// System.out.println("Telefone: " + mario.telefone);
+		// System.out.println("Bigode: " + mario.bigode);
+		// System.out.println("Sapato: " + mario.sapato);
+		// System.out.println("Luvas: " + mario.luvas);
+		// System.out.println("Botões: " + mario.botoes);
+		// System.out.println("Chapeu: " + mario.chapeu + "\n");
+		//
+		// // atribuindo valores as características do objeto Megaman
+		// megaman.nome = "Megaman";
+		// megaman.cor = "azul";
+		// megaman.cabelo = "não tem";
+		// megaman.roupa = "não tem";
+		// megaman.olhos = "azul";
+		// megaman.dataNascimento = "17/12/1987";
+		// megaman.email = "megaman@gmail.com";
+		// megaman.telefone = "1235-1235";
+		// megaman.bigode = false;
+		// megaman.sapato = true;
+		// megaman.luvas = false;
+		// megaman.botoes = false;
+		// megaman.chapeu = false;
+		//
+		// // mostrar valores dos atributos no console
+		// System.out.println("----- Personagem Megaman -----");
+		// System.out.println("Nome: " + megaman.nome);
+		// System.out.println("Cor: " + megaman.cor);
+		// System.out.println("Cabelo: " +megaman.cabelo);
+		// System.out.println("Roupa: " + megaman.roupa);
+		// System.out.println("Olhos: " + megaman.olhos);
+		// System.out.println("Data Nascimento: " + megaman.dataNascimento);
+		// System.out.println("E-mail: " + megaman.email);
+		// System.out.println("Telefone: " + megaman.telefone);
+		// System.out.println("Bigode: " + megaman.bigode);
+		// System.out.println("Sapato: " + megaman.sapato);
+		// System.out.println("Luvas: " + megaman.luvas);
+		// System.out.println("Botões: " + megaman.botoes);
+		// System.out.println("Chapeu: " + megaman.chapeu);
+
+		// após o encapsulamento
 		// atribuindo valores as características do objeto Mario
-		mario.nome = "Mario Bros";
-		mario.cor = "branco";
-		mario.cabelo = "preto";
-		mario.roupa = "vermelha";
-		mario.olhos = "castanhos";
-		mario.dataNascimento = "14/07/1983";
-		mario.email = "mario@gmail.com";
-		mario.telefone = "1234-1234";
-		mario.bigode = true;
-		mario.sapato = true;
-		mario.luvas = true;
-		mario.botoes = true;
-		mario.chapeu = true;
+		mario.setNome("Mario Bros");
+		mario.setCor("branco");
+		mario.setCabelo("preto");
+		mario.setRoupa("vermelha");
+		mario.setOlhos("castanhos");
+		mario.setDataNascimento("14/07/1983");
+		mario.setEmail("mario@gmail.com");
+		mario.setTelefone("1234-1234");
+		mario.setBigode(true);
+		mario.setSapato(true);
+		mario.setLuvas(true);
+		mario.setBotoes(true);
+		mario.setChapeu(true);
 
 		// mostrar valores dos atributos no console
 		System.out.println("----- Personagem Mario -----");
-		System.out.println("Nome: " + mario.nome);
-		System.out.println("Cor: " + mario.cor);
-		System.out.println("Cabelo: " + mario.cabelo);
-		System.out.println("Roupa: " + mario.roupa);
-		System.out.println("Olhos: " + mario.olhos);
-		System.out.println("Data Nascimento: " + mario.dataNascimento);
-		System.out.println("E-mail: " + mario.email);
-		System.out.println("Telefone: " + mario.telefone);
-		System.out.println("Bigode: " + mario.bigode);
-		System.out.println("Sapato: " + mario.sapato);
-		System.out.println("Luvas: " + mario.luvas);
-		System.out.println("Botões: " + mario.botoes);
-		System.out.println("Chapeu: " + mario.chapeu + "\n");
+		System.out.println("Nome: " + mario.getNome());
+		System.out.println("Cor: " + mario.getCor());
+		System.out.println("Cabelo: " + mario.getCabelo());
+		System.out.println("Roupa: " + mario.getRoupa());
+		System.out.println("Olhos: " + mario.getOlhos());
+		System.out.println("Data Nascimento: " + mario.getDataNascimento());
+		System.out.println("E-mail: " + mario.getEmail());
+		System.out.println("Telefone: " + mario.getTelefone());
+		System.out.println("Bigode: " + mario.getBigode());
+		System.out.println("Sapato: " + mario.getSapato());
+		System.out.println("Luvas: " + mario.getLuvas());
+		System.out.println("Botões: " + mario.getBotoes());
+		System.out.println("Chapeu: " + mario.getChapeu() + "\n");
 
 		// atribuindo valores as características do objeto Megaman
-		megaman.nome = "Megaman";
-		megaman.cor = "azul";
-		megaman.cabelo = "não tem";
-		megaman.roupa = "não tem";
-		megaman.olhos = "azul";
-		megaman.dataNascimento = "17/12/1987";
-		megaman.email = "megaman@gmail.com";
-		megaman.telefone = "1235-1235";
-		megaman.bigode = false;
-		megaman.sapato = true;
-		megaman.luvas = false;
-		megaman.botoes = false;
-		megaman.chapeu = false;
+		megaman.setNome("Megaman");
+		megaman.setCor("azul");
+		megaman.setCabelo("não tem");
+		megaman.setRoupa("não tem");
+		megaman.setOlhos("azul");
+		megaman.setDataNascimento("17/12/1987");
+		megaman.setEmail("megaman@gmail.com");
+		megaman.setTelefone("1235-1235");
+		megaman.setBigode(false);
+		megaman.setSapato(true);
+		megaman.setLuvas(false);
+		megaman.setBotoes(false);
+		megaman.setChapeu(false);
 
 		// mostrar valores dos atributos no console
 		System.out.println("----- Personagem Megaman -----");
-		System.out.println("Nome: " + megaman.nome);
-		System.out.println("Cor: " + megaman.cor);
-		System.out.println("Cabelo: " +megaman.cabelo);
-		System.out.println("Roupa: " + megaman.roupa);
-		System.out.println("Olhos: " + megaman.olhos);
-		System.out.println("Data Nascimento: " + megaman.dataNascimento);
-		System.out.println("E-mail: " + megaman.email);
-		System.out.println("Telefone: " + megaman.telefone);
-		System.out.println("Bigode: " + megaman.bigode);
-		System.out.println("Sapato: " + megaman.sapato);
-		System.out.println("Luvas: " + megaman.luvas);
-		System.out.println("Botões: " + megaman.botoes);
-		System.out.println("Chapeu: " + megaman.chapeu);
+		System.out.println("Nome: " + megaman.getNome());
+		System.out.println("Cor: " + megaman.getCor());
+		System.out.println("Cabelo: " + megaman.getCabelo());
+		System.out.println("Roupa: " + megaman.getRoupa());
+		System.out.println("Olhos: " + megaman.getOlhos());
+		System.out.println("Data Nascimento: " + megaman.getDataNascimento());
+		System.out.println("E-mail: " + megaman.getEmail());
+		System.out.println("Telefone: " + megaman.getTelefone());
+		System.out.println("Bigode: " + megaman.getBigode());
+		System.out.println("Sapato: " + megaman.getSapato());
+		System.out.println("Luvas: " + megaman.getLuvas());
+		System.out.println("Botões: " + megaman.getBotoes());
+		System.out.println("Chapeu: " + megaman.getChapeu() + "\n");
+
 	}
 }
