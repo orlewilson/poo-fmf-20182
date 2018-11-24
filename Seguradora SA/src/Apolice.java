ae4b42bd817bded9e1d66b10f86cb28a1676c559
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2018-11-24 00:21:53 -0400

    Adição do exemplo visto no dia 23/11/2018

--- Seguradora SA/src/Apolice.java
+++ Seguradora SA/src/Apolice.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 /*	Programação Orientada a Objeto
  * 	Prof. Orlewilson B. Maia
  * 	Autores: 	
@@ -38,4 +40,78 @@
  */
 public class Apolice {
 
+	// atributos
+	private String nomeSegurado;
+	private int idade;
+	private double valorPremio;
+
+	// construtores
+	public Apolice() {
+		this.nomeSegurado = "";
+		this.idade = 0;
+		this.valorPremio = 0.0;
+	}
+
+	public Apolice(String nomeSegurado, int idade, double valorPremio) {
+		this.nomeSegurado = nomeSegurado;
+		this.idade = idade;
+		this.valorPremio = valorPremio;
+	}
+
+	// métodos
+	public void setNomeSegurado(String nomeSegurado) {
+		this.nomeSegurado = nomeSegurado;
+	}
+
+	public String getNomeSegurado() {
+		return this.nomeSegurado;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getIdade() {
+		return this.idade;
+	}
+
+	public void setValorPremio(double valorPremio) {
+		this.valorPremio = valorPremio;
+	}
+
+	public double getValorPremio() {
+		return this.valorPremio;
+	}
+
+	public void imprimir() {
+		System.out.println("----------------------------------------");
+		System.out.println("Nome Segurado: " + getNomeSegurado());
+		System.out.println("Idade: " + getIdade());
+		System.out.println("Valor Premio: " + getValorPremio());
+		System.out.println("----------------------------------------\n");
+	}
+
+	public void calcularPremioApolice() {
+
+		if (getIdade() >= 18 && getIdade() <= 25) {
+			valorPremio += (valorPremio * 20) / 100;
+		} else if (getIdade() > 25 && getIdade() <= 36) {
+			valorPremio += (valorPremio * 15) / 100;
+		} else if (getIdade() > 36) {
+			valorPremio += (valorPremio * 10) / 100;
+		}
+	}
+
+	public void oferecerDesconto(String cidade) {
+
+		if (cidade.equalsIgnoreCase("manaus")) {
+			valorPremio -= (valorPremio * 20) / 100;
+		} else if (cidade.equalsIgnoreCase("belém")) {
+			valorPremio -= (valorPremio * 15) / 100;
+		} else if (cidade.equalsIgnoreCase("belo horizonte")) {
+			valorPremio -= (valorPremio * 10) / 100;
+		} else if (cidade.equalsIgnoreCase("são paulo")) {
+			valorPremio -= (valorPremio * 5) / 100;
+		}
+	}
 }
--- Seguradora SA/src/PrincipalApolice.java
+++ Seguradora SA/src/PrincipalApolice.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*	Programação Orientada a Objeto
  * 	Prof. Orlewilson B. Maia
  * 	Autores: 	
@@ -30,6 +32,33 @@
 public class PrincipalApolice {
 	
 	public static void main(String[] args) {
+	
+		// a classe Scanner ajuda a ler dados digitados 
+		// no console pelo usuário
+		Scanner lerDadosConsole = new Scanner(System.in);
 		
+		Apolice apolice1 = new Apolice("João", 45, 1000);
+		Apolice apolice2 = new Apolice();
+		
+		apolice1.imprimir();
+		apolice1.calcularPremioApolice();
+		apolice1.imprimir();
+		apolice1.oferecerDesconto("Manaus");
+		apolice1.imprimir();
+		
+		System.out.print("Digite um nome: ");
+		apolice2.setNomeSegurado(lerDadosConsole.nextLine()); // ler conteúdo da linha
+		
+		System.out.print("Digite uma idade: ");
+		apolice2.setIdade(lerDadosConsole.nextInt()); // ler um número inteiro
+		
+		System.out.print("Digite um valor de prêmio: ");
+		apolice2.setValorPremio(lerDadosConsole.nextDouble()); // ler um número real
+		
+		apolice2.imprimir();
+		apolice2.calcularPremioApolice();
+		apolice2.imprimir();
+		apolice2.oferecerDesconto("São Paulo");
+		apolice2.imprimir();
 	}
 }
